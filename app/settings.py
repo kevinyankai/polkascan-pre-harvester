7e07a397e3905dd0e7d5954779b255a90ea59930
kevin <13088966264@163.com>
2020-04-06 22:46:14 +0800

    commit

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,8 @@ DB_CONNECTION = os.environ.get("DB_CONNECTION", "mysql+mysqlconnector://{}:{}@{}
     DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 ))
 
-SUBSTRATE_RPC_URL = os.environ.get("SUBSTRATE_RPC_URL", "http://substrate-node:9933/")
+#SUBSTRATE_RPC_URL = os.environ.get("SUBSTRATE_RPC_URL", "http://substrate-node:9933/")
+SUBSTRATE_RPC_URL = os.environ.get("SUBSTRATE_RPC_URL", "ws://103.224.250.17:3001/")
 SUBSTRATE_ADDRESS_TYPE = int(os.environ.get("SUBSTRATE_ADDRESS_TYPE", 42))
 
 # Simulate Scale encoded extrinsics per block for e.g. performance tests
